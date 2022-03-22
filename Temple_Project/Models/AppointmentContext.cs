31c82b0dbb5cdee5ad577ca8139fb90a8505e51c
dallinks <dallinstirling@gmail.com>
2022-03-22 16:44:20 -0600

    Appointment Context file

---
+++ Temple_Project/Models/AppointmentContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Temple_Project.Models
+{
+    public class AppointmentContext : DbContext
+    {
+        public AppointmentContext(DbContextOptions<AppointmentContext> options) : base(options)
+        {
+        }
+        public DbSet<Group> Groups { get; set; }
+    }
+}
