e6cb848f83e39466a8835a544b5b4b629a3cd73a
gwolf17 <90425429+gwolf17@users.noreply.github.com>
2022-03-22 18:08:50 -0600

    GroupInfoForm

--- Temple_Project/Controllers/HomeController.cs
+++ Temple_Project/Controllers/HomeController.cs
@@ -3,11 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Temple_Project.Models;
 
 namespace Temple_Project.Controllers
 {
     public class HomeController : Controller
     {
+        //
 
+        //Group info page
+        [HttpGet]
+        public IActionResult GroupInfoForm()
+        {
+            return View(new Group());  //Return a new Group object when this page is called
+        }
+
+        [HttpPost]
+        public IActionResult GroupInfoForm()
+        {
+            //Check to see if model state is valid
+            if ModelState.IsValid)
+            {
+
+            }
+        }
     }
 }
